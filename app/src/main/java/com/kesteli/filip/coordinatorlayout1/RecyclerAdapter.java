cf909f4831403694bf8921d3ca712be57c2c3246
Filip Kesteli <filip.kesteli@gmail.com>
2016-08-29 18:34:16 +0200

    CollapsingToolbarLayout

--- app/src/main/java/com/kesteli/filip/coordinatorlayout1/RecyclerAdapter.java
+++ app/src/main/java/com/kesteli/filip/coordinatorlayout1/RecyclerAdapter.java
@@ -1,5 +1,6 @@
 package com.kesteli.filip.coordinatorlayout1;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,36 +14,36 @@ import android.widget.TextView;
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
 
     private String[] titles = {
-        "Chapter One",
-        "Chapter Two",
-        "Chapter Three",
-        "Chapter Four",
-        "Chapter Five",
-        "Chapter Six",
-        "Chapter Seven",
-        "Chapter Eight"
+            "Chapter One",
+            "Chapter Two",
+            "Chapter Three",
+            "Chapter Four",
+            "Chapter Five",
+            "Chapter Six",
+            "Chapter Seven",
+            "Chapter Eight"
     };
 
     private String[] details = {
-        "Item one details",
-        "Item two details",
-        "Item three details",
-        "Item four details",
-        "Item five details",
-        "Item six details",
-        "Item seven details",
-        "Item eight details"
+            "Item one details",
+            "Item two details",
+            "Item three details",
+            "Item four details",
+            "Item five details",
+            "Item six details",
+            "Item seven details",
+            "Item eight details"
     };
 
     private int[] images = {
-        R.drawable.ic_account_circle_black_24dp,
-        R.drawable.ic_account_circle_black_24dp,
-        R.drawable.ic_account_circle_black_24dp,
-        R.drawable.ic_account_circle_black_24dp,
-        R.drawable.ic_account_circle_black_24dp,
-        R.drawable.ic_account_circle_black_24dp,
-        R.drawable.ic_account_circle_black_24dp,
-        R.drawable.ic_account_circle_black_24dp
+            R.drawable.ic_account_circle_black_24dp,
+            R.drawable.ic_account_circle_black_24dp,
+            R.drawable.ic_account_circle_black_24dp,
+            R.drawable.ic_account_circle_black_24dp,
+            R.drawable.ic_account_circle_black_24dp,
+            R.drawable.ic_account_circle_black_24dp,
+            R.drawable.ic_account_circle_black_24dp,
+            R.drawable.ic_account_circle_black_24dp
     };
 
     @Override
@@ -76,6 +77,14 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
             itemImage = (ImageView) itemView.findViewById(R.id.item_image);
             itemTitle = (TextView) itemView.findViewById(R.id.item_title);
             itemDetail = (TextView) itemView.findViewById(R.id.item_detail);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int position = getAdapterPosition();
+                    Snackbar.make(view, "Click detected on item " + position, Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                }
+            });
         }
     }
 
--- app/src/main/java/com/kesteli/filip/coordinatorlayout1/RecyclerViewAndCardViewActivity.java
+++ app/src/main/java/com/kesteli/filip/coordinatorlayout1/RecyclerViewAndCardViewActivity.java
@@ -1,22 +1,41 @@
 package com.kesteli.filip.coordinatorlayout1;
 
+import android.graphics.Color;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 public class RecyclerViewAndCardViewActivity extends AppCompatActivity {
 
-    RecyclerView recyclerView;
-    RecyclerView.LayoutManager layoutManager;
-    RecyclerView.Adapter adapter;
+    private Toolbar toolbar;
+    private CollapsingToolbarLayout collapsingToolbarLayout;
+
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private RecyclerView.Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_view_and_card_view);
 
+        setupToolbar();
+        setupCollapsingToolbarLayout();
         setupRecyclerView();
+    }
+
+    private void setupCollapsingToolbarLayout() {
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbarLayout.setTitle("My Toolbar Title");
+        collapsingToolbarLayout.setContentScrimColor(Color.GREEN);
+    }
+
+    private void setupToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     private void setupRecyclerView() {
@@ -27,3 +46,5 @@ public class RecyclerViewAndCardViewActivity extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
     }
 }
+
+
